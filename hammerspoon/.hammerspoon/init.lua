07014de9ea43b58b94b694049695b9edda1fa856
Goh Wei Wen <goweiwen@gmail.com>
2018-03-18 20:44:34 +0800

    hammerspoon: Update config

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -73,6 +73,8 @@ bind(one, '/', hs.grid.toggleShow)
 
 -- Hotkeys
 bind(one, 'm', hs.grid.maximizeWindow)
+bind(one, 'b', function() hs.window.focusedWindow():centerOnScreen():setFrameInScreenBounds() end)
+bind(one, 'c', function() hs.dialog.color.show() end)
 
 -- Focus window
 bind(one, 'h', function() hs.window.focusedWindow():focusWindowWest() end)
@@ -89,37 +91,30 @@ bind(two, 'E', hs.grid.pushWindowNextScreen)
 bind(two, 'R', hs.grid.pushWindowPrevScreen)
 
 -- Move window to spaces (hacky)
-function pushWindowNextSpace()
+function moveWindowNextSpace() moveWindowRelativeSpace(1) end
+function moveWindowPrevSpace() moveWindowRelativeSpace(-1) end
+
+function moveWindowRelativeSpace(n)
   local allSpaces = spaces.query()
   local activeSpace = spaces.activeSpace()
   for i, v in ipairs(allSpaces) do
     if v == activeSpace then
-      return hs.window.focusedWindow():spacesMoveTo(allSpaces[(i - 2) % #allSpaces + 1])
+      return hs.window.focusedWindow():spacesMoveTo(allSpaces[(i - n - 1) % #allSpaces + 1])
     end
   end
 end
 
-function pushWindowPrevSpace()
-  local allSpaces = spaces.query()
-  local activeSpace = spaces.activeSpace()
-  for i, v in ipairs(allSpaces) do
-    if v == activeSpace then
-      return hs.window.focusedWindow():spacesMoveTo(allSpaces[i % #allSpaces + 1])
-    end
-  end
-end
-
-function pushWindowToSpace(n)
+function moveWindowToSpace(n)
   local allSpaces = spaces.query()
   local activeSpace = spaces.activeSpace()
   return hs.window.focusedWindow():spacesMoveTo(allSpaces[#allSpaces - n + 1])
 end
 
-bind('ctrl-shift', 'left', nil, compose2(pushWindowPrevSpace, keyStroke('ctrl', 'left')))
-bind('ctrl-shift', 'right', nil, compose2(pushWindowNextSpace, keyStroke('ctrl', 'right')))
+bind('ctrl-shift', 'left', nil, compose2(moveWindowPrevSpace, keyStroke('ctrl', 'left')))
+bind('ctrl-shift', 'right', nil, compose2(moveWindowNextSpace, keyStroke('ctrl', 'right')))
 for i = 1, 9 do
   bind('ctrl-shift', tostring(i), nil, compose2(
-    function() pushWindowToSpace(i) end,
+    function() moveWindowToSpace(i) end,
     keyStroke('ctrl', tostring(i))
   ))
 end
@@ -133,6 +128,18 @@ resizeMode:bind('', 'l', hs.grid.resizeWindowWider)
 resizeMode:bind('', 'Escape', function() resizeMode:exit() end)
 resizeMode:bind('', 'Return', function() resizeMode:exit() end)
 
+-- Mouse follows Focus
+local allWindowsFilter = hs.window.filter.new()
+allWindowsFilter:subscribe(hs.window.filter.windowFocused, function(window)
+  local size = window:size()
+  local position = window:topLeft()
+  local center = {
+    x = position.x + size.w / 2,
+    y = position.y + size.h / 2,
+  }
+  hs.mouse.setAbsolutePosition(center)
+end)
+
 -- Trackpad Gestures
 -- bind(one, 'up', execute('chunkc tiling:monitor -f prev'))
 -- bind(one, 'down', execute('chunkc tiling:monitor -f next'))
@@ -142,7 +149,7 @@ bind(one, 'left', nil, keyStroke('ctrl', 'right'))
 bind(one, 'right', nil, keyStroke('ctrl', 'left'))
 
 -- Vim bindings
-local vim = hs.hotkey.modal.new(one, 'escape')
+local vim = hs.hotkey.modal.new(one, 'i')
 vim:bind('', 'escape', function() vim:exit() end)
   :bind('', 'd', keyStroke('', 'space'), nil, keyStroke('', 'space'))
   :bind('', 'e', keyStroke('shift', 'space'), nil, keyStroke('shift', 'space'))
@@ -150,36 +157,5 @@ vim:bind('', 'escape', function() vim:exit() end)
   :bind('', 'j', keyStroke('', 'down'), nil, keyStroke('', 'down'))
   :bind('', 'k', keyStroke('', 'up'), nil, keyStroke('', 'up'))
   :bind('', 'l', keyStroke('', 'right'), nil, keyStroke('', 'right'))
-
--- Mouse
-function mouseMove(x, y)
-  return function()
-    local point = hs.mouse.getAbsolutePosition()
-    hs.mouse.setAbsolutePosition({ x = point.x + x, y = point.y + y })
-  end
-end
-
-function scrollWheel(x, y)
-  return function()
-    hs.eventtap.scrollWheel({ x, y }, {})
-  end
-end
-
-local mouse = hs.hotkey.modal.new(one, '\\')
-mouse:bind('', 'escape', function() mouse:exit() end)
-  :bind('', 'h', mouseMove(-10, 0), nil, mouseMove(-10, 0))
-  :bind('', 'j', mouseMove(0, 10), nil, mouseMove(0, 10))
-  :bind('', 'k', mouseMove(0, -10), nil, mouseMove(0, -10))
-  :bind('', 'l', mouseMove(10, 0), nil, mouseMove(10, 0))
-  :bind('shift', 'h', mouseMove(-50, 0), nil, mouseMove(-50, 0))
-  :bind('shift', 'j', mouseMove(0, 50), nil, mouseMove(0, 50))
-  :bind('shift', 'k', mouseMove(0, -50), nil, mouseMove(0, -50))
-  :bind('shift', 'l', mouseMove(50, 0), nil, mouseMove(50, 0))
-  :bind('', 'space', function() hs.eventtap.leftClick(hs.mouse.getAbsolutePosition()) end)
-  :bind('shift', 'space', function() hs.eventtap.rightClick(hs.mouse.getAbsolutePosition()) end)
-  :bind('', 'e', scrollWheel(0, 3), nil, scrollWheel(0, 3))
-  :bind('', 'd', scrollWheel(0, -3), nil, scrollWheel(0, -3))
-  :bind('shift', 'e', scrollWheel(0, 15), nil, scrollWheel(0, 15))
-  :bind('shift', 'd', scrollWheel(0, -15), nil, scrollWheel(0, -15))
 
 hs.alert('Config loaded.')
