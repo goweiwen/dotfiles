ac74d1ee8dcbedf27836ed678baf312b2346b758
Goh Wei Wen <goweiwen@gmail.com>
2018-03-29 23:22:38 +0800

    emacs: Move stuff from init to config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -30,6 +30,9 @@
 (setq user-full-name "Goh Wei Wen"
       user-mail-address "goweiwen@gmail.com")
 
+;; Secrets
+(load "~/.emacs.d/secrets.el")
+
 ;; Persistent Server
 ;; (use-package mac-pseudo-daemon
 ;;   :init
@@ -47,6 +50,11 @@
 (defun edit-init ()
   (interactive)
   (switch-to-buffer (find-file-noselect "~/.emacs.d/init.el")))
+
+;; Edit todo.org
+(defun edit-todo ()
+  (interactive)
+  (switch-to-buffer (find-file-noselect "~/Notes/todo.org")))
 
 ;; ;; Hot reloading init.el
 ;; (defun hot-reload-init ()
@@ -104,7 +112,7 @@
 
 ;; Tweak window chrome
 (tool-bar-mode 0)
-(menu-bar-mode 0)
+(menu-bar-mode 1)
 (when window-system
   (scroll-bar-mode -1))
 
@@ -119,27 +127,30 @@
         dashboard-items '((recents . 5)
                           (bookmarks . 5)
                           (projects . 5)
-                          (agenda . 5))))
+                          (agenda))))
 
 ;; Frame title
 (setq frame-title-format '((:eval (projectile-project-name))))
 
 ;; Hide a few minor modes
 (diminish 'undo-tree-mode)
-(diminish 'flycheck-mode)
-(diminish 'projectile-mode)
 
 ;; Mode line
 (use-package telephone-line
   :after evil
   :config
+  (setq telephone-line-primary-left-separator 'telephone-line-nil
+        telephone-line-secondary-left-separator 'telephone-line-nil
+        telephone-line-primary-right-separator 'telephone-line-nil
+        telephone-line-secondary-right-separator 'telephone-line-nil)
   (setq telephone-line-lhs
         '((evil   . (telephone-line-evil-tag-segment))
-          (accent . (telephone-line-major-mode-segment))
+          (accent . (telephone-line-vc-segment))
           (nil    . (telephone-line-buffer-segment))))
   (setq telephone-line-rhs
-        '((nil    . (telephone-line-misc-info-segment))
-          (accent . (telephone-line-vc-segment
+        '((nil    . (telephone-line-major-mode-segment))
+          (accent . (telephone-line-minor-mode-segment
+                     telephone-line-misc-info-segment
                      telephone-line-erc-modified-channels-segment
                      telephone-line-process-segment))
           (evil   . (telephone-line-airline-position-segment))))
@@ -148,6 +159,9 @@
 ;; Terminal stuff
 (xterm-mouse-mode 1)
 (normal-erase-is-backspace-mode 1)
+
+;; Emacs is not a window manager
+(use-package frames-only-mode)
 
 ;; ;; Mouse scrolling
 ;; (setq mac-mouse-wheel-smooth-scroll nil
@@ -164,34 +178,34 @@
 ;; ===
 
 (use-package evil
-  :init
+  :config
   (evil-mode 1))
 
 ;; evil-surround
 (use-package evil-surround
-  :init
+  :config
   (global-evil-surround-mode 1))
 
 ;; evil-commentary
 (use-package evil-commentary
   :diminish evil-commentary-mode
-  :init
+  :config
   (evil-commentary-mode))
 
 ;; evil-snipe (vim-sneak)
 (use-package evil-snipe
   :diminish evil-snipe-local-mode
-  :init
+  :config
   (evil-snipe-mode 1)
   (add-hook 'magit-mode-hook 'turn-off-evil-snipe-override-mode)
-  :config
   (setq evil-snipe-scope 'visible
         evil-snipe-repeat-scope 'whole-visible
         evil-snipe-spillover-scope 'buffer))
 
 ;; Multiple cursors
 (use-package evil-mc
-  :init
+  :diminish
+  :config
   (global-evil-mc-mode 1))
 
 ;; evil-arg
@@ -209,7 +223,7 @@
 ;; Emacs bindings in evil-mode
 (use-package evil-rsi
   :diminish evil-rsi-mode
-  :init
+  :config
   (evil-rsi-mode))
 
 ;; bind evil-jump-out-args
@@ -220,7 +234,7 @@
 
 ;; folding
 (use-package vimish-fold
-  :init
+  :config
   (vimish-fold-global-mode 1))
 
 ;; Visual sticky shift indentation
@@ -247,9 +261,8 @@
 ;; which-key
 (use-package which-key
   :diminish which-key-mode
-  :init
-  (which-key-mode)
   :config
+  (which-key-mode)
   (which-key-setup-side-window-bottom)
   (setq which-key-idle-delay 0.2))
 
@@ -270,6 +283,7 @@
 (define-key leader-map "bb" 'switch-to-buffer)
 (define-key leader-map "bd" 'kill-this-buffer)
 (define-key leader-map "fe" 'edit-init)
+(define-key leader-map "ft" 'edit-todo)
 (define-key leader-map "ff" 'counsel-find-file)
 (define-key leader-map "fp" 'counsel-git)
 (define-key leader-map "fr" 'counsel-recentf)
@@ -277,6 +291,13 @@
 (define-key leader-map "fv" 'counsel-ag)
 (define-key leader-map "g" 'magit-status)
 (define-key leader-map "h" 'counsel-describe-variable)
+(define-key leader-map "oa" 'org-agenda)
+(define-key leader-map "oc" 'cfw:open-org-calendar)
+(define-key leader-map "ol" 'org-agenda-list)
+(define-key leader-map "ob" 'org-iswitchb)
+(define-key leader-map "c" 'org-capture)
+(define-key leader-map "oe" 'edit-todo)
+(define-key leader-map "ol" 'org-store-link)
 (define-key leader-map "pb" 'counsel-projectile-switch-to-buffer)
 (define-key leader-map "pc" 'projectile-compile-project)
 (define-key leader-map "pd" 'projectile-dired)
@@ -332,9 +353,8 @@
 
 ;; Line numbers
 (use-package nlinum-relative
-  :init
-  (add-hook 'prog-mode-hook 'nlinum-mode)
-  (add-hook 'prog-mode-hook 'nlinum-relative-mode)
+  :hook ((prog-mode-hook nlinum-mode)
+         (prog-mode-hook nlinum-relative-mode))
   :config
   (nlinum-relative-setup-evil)
   (setq nlinum-format " %d ")
@@ -343,6 +363,7 @@
 
 ;; Git Gutter
 (use-package git-gutter
+  :diminish
   :config
   (global-git-gutter-mode +1)
   (git-gutter:linum-setup))
@@ -376,30 +397,25 @@
 
 ;; Smooth scrolling
 (use-package smooth-scrolling
-  :init
+  :config
   (smooth-scrolling-mode 1))
 
 ;; Autocompletion
 (use-package company
-  :init
-  (add-hook 'after-init-hook 'global-company-mode))
+  :diminish
+  :hook (after-init-hook global-company-mode))
 
 ;; Code highlighting
 (use-package highlight-numbers
-  :init
-  (add-hook 'prog-mode-hook #'highlight-numbers-mode))
+  :hook (prog-mode-hook highlight-numbers-mode))
 (use-package highlight-quoted
-  :init
-  (add-hook 'prog-mode-hook #'highlight-quoted-mode))
+  :hook (prog-mode-hook highlight-quoted-mode))
 (use-package highlight-defined
-  :init
-  (add-hook 'prog-mode-hook #'highlight-defined-mode))
+  :hook (prog-mode-hook highlight-defined-mode))
 (use-package highlight-operators
-  :init
-  (add-hook 'prog-mode-hook #'highlight-operators-mode))
+  :hook (prog-mode-hook highlight-operators-mode))
 (use-package highlight-escape-sequences
-  :init
-  (add-hook 'prog-mode-hook #'hes-mode))
+  :hook (prog-mode-hook hes-mode))
 
 ;; Matching parenthesis
 (show-paren-mode 1)
@@ -407,35 +423,40 @@
 
 ;; Linting with Flycheck
 (use-package flycheck
-  :diminish FlyCheck
-  :init
-  (add-hook 'prog-mode-hook 'flycheck-mode)
+  ;; :diminish
+  :hook (prog-mode-hook flycheck-mode)
   :config
+
+  ;; ;; Flycheck
+  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+  (flycheck-add-mode 'javascript-eslint 'js2-mode)
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-mode 'javascript-eslint 'vue-mode)
+
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers
                         '(javascript-jshint)))
   (use-package flycheck-pos-tip
-    :init
-    (add-hook 'flycheck-mode-hook 'flycheck-pos-tip-mode))
+    :hook (flycheck-mode-hook flycheck-pos-tip-mode))
   (use-package flycheck-color-mode-line
-    :init
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+    :hook (flycheck-mode-hook flycheck-color-mode-line-mode)))
 
 ;; Snippets
 (use-package yasnippet
   :diminish yas-global-mode yas-minor-mode
-  :init (add-hook 'after-init-hook 'yas-global-mode)
+  :hook (after-init-hook yas-global-mode)
   :config (setq yas-snippet-dirs '("~/.emacs.d/snippets/")))
 
 ;; Projectile
 ;; ===
 
 (use-package projectile
-  :init
-  (projectile-mode)
+  :diminish
   :config
+  (projectile-mode)
   (setq projectile-switch-project-action 'projectile-find-file
-        projectile-use-git-grep t)
+        projectile-use-git-grep t
+        projectile-completion-system 'ivy)
   (define-key evil-normal-state-map "\C-p" 'projectile-find-file))
 
 (use-package counsel-projectile
@@ -447,9 +468,8 @@
 ;; ===
 (use-package ivy
   :diminish ivy-mode
-  :init
-  (ivy-mode 1)
   :config
+  (ivy-mode 1)
   (setq ivy-use-virtual-buffers t
         enable-recursive-minibuffers t)
   (add-to-list 'ivy-ignore-buffers "\\*Messages\\*")
@@ -495,9 +515,8 @@
 
 ;; Highlight uncommitted changes
 (use-package diff-hl
-  :init
-  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
+  :hook ((prog-mode-hook turn-on-diff-hl-mode)
+         (vc-dir-mode-hook turn-on-diff-hl-mode)))
 
 ;; Python
 ;; ===
@@ -545,6 +564,9 @@
                 web-mode-css-indent-offset 2
                 web-mode-code-indent-offset 2
                 web-mode-attr-indent-offset 2))
+
+;; Preprocessors
+(use-package pug-mode)
 
 ;; CSS
 ;; ===
@@ -621,29 +643,35 @@
 ;; Vue
 (use-package vue-mode
   :mode ("\\.vue\\'" . vue-mode)
-  :init
   :config
   (setq mmm-submode-decoration-level 0))
-
-;; Flycheck
-(require 'flycheck)
-(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
-(flycheck-add-mode 'javascript-eslint 'js2-mode)
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-(flycheck-add-mode 'javascript-eslint 'vue-mode)
 
 ;; Org
 ;; ===
 
 (use-package org
-  ;; :mode ("\\.org\\" . org-mode)
+  :mode ("\\.org\\'" . org-mode)
   :ensure org-plus-contrib
-  :init
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (flyspell-mode)
-              (org-bullets-mode t)))
+  :hook (org-mode-hook flyspell-mode)
   :config
+  (setq org-agenda-files (quote ("~/Notes/todo.org" "~/Notes/inbox.org" "~/Notes/calendar.org")))
+  
+  (setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                   (org-agenda-files :maxlevel . 9))))
+
+  (setq org-todo-keywords
+        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+                (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+  (setq org-capture-templates
+        (quote (("t" "todo" entry (file "~/Notes/inbox.org")
+                 "* TODO %?"))))
+
+  (set-face-attribute 'org-level-1 nil :weight 'bold :height 1.20)
+  (set-face-attribute 'org-level-2 nil :weight 'bold :height 1.15)
+  (set-face-attribute 'org-level-3 nil :weight 'bold :height 1.10)
+  (set-face-attribute 'org-level-4 nil :weight 'bold :height 1.05)
+  (set-face-attribute 'org-level-5 nil :weight 'bold :height 1.00)
+
   (setq org-ellipsis " ▼"
         org-src-fontify-natively t
         org-src-tab-acts-natively t
@@ -655,53 +683,79 @@
         org-startup-with-latex-preview t
         org-format-latex-options (plist-put org-format-latex-options :scale 0.8)
         org-startup-with-inline-images t)
+
   (define-key org-mode-map (kbd "$") (lambda ()
                                        (interactive)
                                        (insert "$")
-                                       (save-excursion
-                                         (left-char 1)
-                                         (if (org-inside-LaTeX-fragment-p)
-                                             (progn
-                                               (right-char 2)
-                                               (org-preview-latex-fragment)))))))
+                                       (left-char 1)
+                                       (org-toggle-latex-fragment)
+                                       (right-char 1))))
+
+;; Calendar
+(use-package calfw)
+(use-package calfw-org
+  :after calfw)
+
+(use-package org-gcal
+  :config
+  (setq org-gcal-client-id "79887467989-4inb8t4kbrmqmb7453edv7i9bagsr673.apps.googleusercontent.com"
+        org-gcal-file-alist '(("goweiwen@gmail.com" . "~/Notes/calendar.org"))))
+
 
 ;; Coq
 ;; ===
 (use-package company-coq
   :mode (("\\.v\\'" . company-coq-mode)
          ("\\.v\\'" . coq-mode))
+  :hook ((company-coq-mode . coq-symbols-list)
+         (coq-mode . coq-symbols-list))
+  :bind (:map company-coq-map
+              ("M-j" . proof-assert-next-command-interactive)
+              ("M-k" . proof-undo-last-successful-command)
+              ("M-<return>" . proof-goto-point))
+  :custom-face
+  (proof-locked-face ((nil (:background "#003300"))))
+  (proof-queue-face ((nil (:background "#116611"))))
   :config
+
+  ;; Appearance
+
+  ;; Use Coq 8.6 (for CS3234)
   (setenv "PATH" (concat (getenv "PATH") "/Users/weiwen/.opam/4.05.0/bin"))
   (setq exec-path (append exec-path '("/Users/weiwen/.opam/4.05.0/bin")))
-  (setq prettify-symbols-alist
-        (quote
-         (("|-" . 8866)
-          ("||" . 8214)
-          ("/\\" . 8743)
-          ("\\/" . 8744)
-          ("->" . 8594)
-          ("<-" . 8592)
-          ("<->" . 8596)
-          ("=>" . 8658)
-          ("<=" . 8804)
-          (">=" . 8805)
-          ("True" . 8868)
-          ("False" . 8869)
-          ("fun" . 955)
-          ("forall" . 8704)
-          ("exists" . 8707)
-          ("nat" . 8469)
-          ("Prop" . 8473)
-          ("Real" . 8477)
-          ("bool" . 120121)
-          (">->" . 8611)
-          ("-->" . 10230)
-          ("<--" . 10229)
-          ("<-->" . 10231)
-          ("==>" . 10233)
-          ("<==" . 10232)
-          ("~~>" . 10239)
-          ("<~~" . 11059)))))
+
+  ;; Prettify symbols
+  (setq coq-symbols-list
+        '(lambda()
+           (setq prettify-symbols-alist
+                 (quote
+                  (("|-" . 8866)
+                   ("||" . 8214)
+                   ("/\\" . 8743)
+                   ("\\/" . 8744)
+                   ("->" . 8594)
+                   ("<-" . 8592)
+                   ("<->" . 8596)
+                   ("=>" . 8658)
+                   ("<=" . 8804)
+                   (">=" . 8805)
+                   ("True" . 8868)
+                   ("False" . 8869)
+                   ("fun" . 955)
+                   ("forall" . 8704)
+                   ("exists" . 8707)
+                   ("nat" . 8469)
+                   ("Prop" . 8473)
+                   ("Real" . 8477)
+                   ("bool" . 120121)
+                   (">->" . 8611)
+                   ("-->" . 10230)
+                   ("<--" . 10229)
+                   ("<-->" . 10231)
+                   ("==>" . 10233)
+                   ("<==" . 10232)
+                   ("~~>" . 10239)
+                   ("<~~" . 11059)))))))
 
 ;; Export
 (require 'ox-md)
@@ -734,26 +788,25 @@
 ;;   (setq darkroom-text-scale-increase 0)
 ;;   (add-hook 'org-mode-hook 'darkroom-tentative-mode))
 
-;; ;; Use variable pitch for text modes
-;; (use-package mixed-pitch
-;;   :init
-;;   (add-hook 'org-mode-hook 'mixed-pitch-mode))
+;; Use variable pitch for text modes
+(use-package mixed-pitch
+  :hook ('org-mode-hook 'mixed-pitch-mode))
 
-;; Proselint
-(require 'flycheck)
-(flycheck-define-checker proselint
-  "A linter for prose."
-  :command ("proselint" source-inplace)
-  :error-patterns
-  ((warning line-start (file-name) ":" line ":" column ": "
-            (id (one-or-more (not (any " "))))
-            (message (one-or-more not-newline)
-                     (zero-or-more "\n" (any " ") (one-or-more not-newline)))
-            line-end))
-  :modes (text-mode markdown-mode gfm-mode org-mode))
-(add-to-list 'flycheck-checkers 'proselint)
+;; ;; Proselint
+;; (require 'flycheck)
+;; (flycheck-define-checker proselint
+;;   "A linter for prose."
+;;   :command ("proselint" source-inplace)
+;;   :error-patterns
+;;   ((warning line-start (file-name) ":" line ":" column ": "
+;;             (id (one-or-more (not (any " "))))
+;;             (message (one-or-more not-newline)
+;;                      (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+;;             line-end))
+;;   :modes (text-mode markdown-mode gfm-mode org-mode))
+;; (add-to-list 'flycheck-checkers 'proselint)
 
-(add-hook 'text-mode-hook #'flycheck-mode)
+;; (add-hook 'text-mode-hook #'flycheck-mode)
 
 ;; Automatically generated
 
@@ -764,6 +817,6 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nlinum-relative evil-visual-mark-mode company-coq markdown-mode elm-mode vue-mode wc-mode ranger olivetti olivetti-mode writeroom-mode minimap evil-snipe evil-mc company-anaconda py-yapf ag))))
+    (frames-only-mode mixed-pitch calfw-org calfw mode-icons nlinum-relative evil-visual-mark-mode company-coq markdown-mode elm-mode vue-mode wc-mode ranger olivetti olivetti-mode writeroom-mode minimap evil-snipe evil-mc company-anaconda py-yapf ag))))
 
 ;;; init.el ends here
