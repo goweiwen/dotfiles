a60b68509142a1d6a5538e266e2ff9b9393b38cc
Goh Wei Wen <goweiwen@gmail.com>
2018-06-16 23:54:48 +0800

    doom-emacs: Enable natural titlebar

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -1,14 +1,18 @@
 ;;; private/ww/config.el -*- lexical-binding: t; -*-
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(menu-bar-mode 1)
+
 (setq doom-font (font-spec :family "Fira Code" :size 14))
 
 ;;
 ;; Coq - Proof General
 ;;
 
-(def-package! company-coq
-  :config
-  (add-hook 'coq-mode-hook #'company-coq-mode))
+;; (def-package! company-coq
+;;   :config
+;;   (add-hook 'coq-mode-hook #'company-coq-mode))
 
-(setenv "PATH" (concat (getenv "PATH") "/Users/weiwen/.opam/4.05.0/bin"))
-(setq exec-path (append exec-path '("/Users/weiwen/.opam/4.05.0/bin")))
+;; (setenv "PATH" (concat (getenv "PATH") "/Users/weiwen/.opam/4.05.0/bin"))
+;; (setq exec-path (append exec-path '("/Users/weiwen/.opam/4.05.0/bin")))
